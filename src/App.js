77a27986e7f02c141b0d682a3f2a5ea3713e0e32
Alan <alan_hahn1995@outlook.com>
2025-02-17 13:41:37 -0300

    Finalizado o componente de Navbar

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
 
 import Container from './components/layout/Container';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/company">company</Link>
-        <Link to="/contatc">contatc</Link>
-        <Link to="/newproject">newproject</Link>
-      </div>
+      <Navbar />
       
       <Container customClass="min-height">
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/company" element={<Company />} />
+          <Route exact path="/projects" element={<Projects />} />
           <Route exact path="/contatc" element={<Contact />} />
           <Route exact path="/newproject" element={<NewProject />} /> 
         </Routes>   
       </Container>
       
-      <footer>Footer</footer>
+      <Footer />
     </Router>
   );
 }
